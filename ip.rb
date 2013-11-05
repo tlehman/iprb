f01840fbd6845f6fb961c03142548125075dc7d2
Tobi Lehman <tobi.lehman@gmail.com>
2013-11-05 10:06:55 -0800

    Add ==, to_i, from_i

--- ip.rb
+++ ip.rb
@@ -1,3 +1,19 @@
 class IP
+  def initialize(ip_str)
+    @octets = ip_str.split(".").map(&:to_i)
+  end
+
+  def ==(ip)
+    self.to_i == ip.to_i
+  end
+
+  def to_i
+    @octets.each_with_index.map { |o,i| o*(256**(3-i)) }.inject(:+)
+  end
+
+  def self.from_i(i)
+    ip_str = i.to_s(16).rjust(8, "0").scan(/../).map { |s| s.to_i(16).to_s }.join(".")
+    return self.new(ip_str)
+  end
 end
 
--- ip_test.rb
+++ ip_test.rb
@@ -1,8 +1,18 @@
 require 'test/unit'
+require './ip.rb'
 
 class TestIP < Test::Unit::TestCase
-  def equals_test
+  def test_equals
     assert_equal IP.new("192.168.0.110"), IP.new("192.168.0.110")
+  end
+
+  def test_to_i
+    assert_equal IP.new("0.0.0.0").to_i, 0x00000000
+    assert_equal IP.new("255.255.255.255").to_i, 0xffffffff
+  end
+
+  def test_inverse
+    assert_equal IP.new("10.1.10.13"), IP.from_i(IP.new("10.1.10.13").to_i)
   end
 end
 
